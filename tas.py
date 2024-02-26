d927889e01c8537a3b602fdfc6c3b51d47683275
GhazalToarbi <ghazaltorabi020@gmail.com>
2024-02-26 22:08:23 +0330

    ddddeeellltee

--- tas.py
+++
@@ -1,7 +0,0 @@
-import random
-list1 = [0, 0 , 0 , 0 , 0, 0, 0]
-for i in range(10):
-    list1[random.randrange(1,7)] +=1
-print  ("number","         freuency")
-for i in range(1,7):
-    print(f'{i:3} {list1[i]:17}')
